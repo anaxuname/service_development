eed3781ad765ba5cb0f3fad42d36a94c721ddeb8
Anaxuname <anaxuname@gmail.com>
2024-02-06 21:17:37 +0300

    added cache

--- service_development/settings.py
+++ service_development/settings.py
@@ -144,3 +144,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {"BACKEND": "django.core.cache.backends.redis.RedisCache", "LOCATION": "redis://127.0.0.1:6379", }}
